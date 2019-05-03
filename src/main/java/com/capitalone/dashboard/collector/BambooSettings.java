b3384a8aa1531f43eea815eadeab69cf2353492d
Tompkins <christopher.tompkins@capitalone.com>
2019-05-03 08:46:30 -0400

    (refactor) more checkstyle fixes

--- src/main/java/com/capitalone/dashboard/collector/BambooSettings.java
+++ src/main/java/com/capitalone/dashboard/collector/BambooSettings.java
@@ -30,76 +30,80 @@ import java.util.List;
 public class BambooSettings {
 
 
-    private String cron;
-    private boolean saveLog = false;
-    private List<String> servers;
-    private List<String> niceNames;
-    private String username;
-    private String apiKey;
-    private String dockerLocalHostIP; //null if not running in docker on http://localhost
+  private String cron;
+  private boolean saveLog = false;
+  private List<String> servers;
+  private List<String> niceNames;
+  private String username;
+  private String apiKey;
+  private String dockerLocalHostIP; //null if not running in docker on http://localhost
 
-    public String getCron() {
-        return cron;
-    }
+  public String getCron() {
+    return cron;
+  }
 
-    public void setCron(String cron) {
-        this.cron = cron;
-    }
+  public void setCron(String cron) {
+    this.cron = cron;
+  }
 
-    public boolean isSaveLog() {
-        return saveLog;
-    }
+  public boolean isSaveLog() {
+    return saveLog;
+  }
 
-    public void setSaveLog(boolean saveLog) {
-        this.saveLog = saveLog;
-    }
+  public void setSaveLog(boolean saveLog) {
+    this.saveLog = saveLog;
+  }
 
-    public List<String> getServers() {
-        return servers;
-    }
+  public List<String> getServers() {
+    return servers;
+  }
 
-    public void setServers(List<String> servers) {
-        this.servers = servers;
-    }
+  public void setServers(List<String> servers) {
+    this.servers = servers;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public String getApiKey() {
-        return apiKey;
-    }
+  public String getApiKey() {
+    return apiKey;
+  }
 
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
-    }
-    
-    public void setDockerLocalHostIP(String dockerLocalHostIP) {
-        this.dockerLocalHostIP = dockerLocalHostIP;
-    }
+  public void setApiKey(String apiKey) {
+    this.apiKey = apiKey;
+  }
 
-    public List<String> getNiceNames() {
-        return niceNames;
-    }
+  public void setDockerLocalHostIP(String dockerLocalHostIP) {
+    this.dockerLocalHostIP = dockerLocalHostIP;
+  }
 
-    public void setNiceNames(List<String> niceNames) {
-        this.niceNames = niceNames;
-    }
+  public List<String> getNiceNames() {
+    return niceNames;
+  }
 
-	//Docker NATs the real host localhost to 10.0.2.2 when running in docker
-	//as localhost is stored in the JSON payload from jenkins we need
-	//this hack to fix the addresses
-    public String getDockerLocalHostIP() {
-    	
-    		//we have to do this as spring will return NULL if the value is not set vs and empty string
-    	String localHostOverride = "";
-    	if (dockerLocalHostIP != null) {
-    		localHostOverride = dockerLocalHostIP;
-    	}
-        return localHostOverride;
+  public void setNiceNames(List<String> niceNames) {
+    this.niceNames = niceNames;
+  }
+
+  /**
+   * Docker NATs the real host localhost to 10.0.2.2 when running in docker
+   * as localhost is stored in the JSON payload from jenkins we need
+   * this hack to fix the addresses.
+   *
+   * @return the IPAddress for the docker localhost.
+   */
+  public String getDockerLocalHostIP() {
+
+    //we have to do this as spring will return NULL if the value is not set vs and empty string
+    String localHostOverride = "";
+    if (dockerLocalHostIP != null) {
+      localHostOverride = dockerLocalHostIP;
     }
+    return localHostOverride;
+  }
 }
