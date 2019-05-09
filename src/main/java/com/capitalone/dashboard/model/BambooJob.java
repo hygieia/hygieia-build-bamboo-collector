123b25e3027775fdf7f68382c6c3ea557f5c5eb1
Tompkins <christopher.tompkins@capitalone.com>
2019-05-09 09:50:15 -0400

    (fix) all checkstyle errors

--- src/main/java/com/capitalone/dashboard/model/BambooJob.java
+++ src/main/java/com/capitalone/dashboard/model/BambooJob.java
@@ -21,24 +21,25 @@ package com.capitalone.dashboard.model;
  * CollectorItem extension to store the instance, build job and build url.
  */
 public class BambooJob extends JobCollectorItem {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-        	return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-        	return false;
-        }
-
-        BambooJob bambooJob = (BambooJob) o;
-
-        return getInstanceUrl().equals(bambooJob.getInstanceUrl()) && getJobName().equals(bambooJob.getJobName());
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
     }
 
-    @Override
-    public int hashCode() {
-        int result = getInstanceUrl().hashCode();
-        result = 31 * result + getJobName().hashCode();
-        return result;
-    }
+    BambooJob bambooJob = (BambooJob) obj;
+
+    return getInstanceUrl().equals(bambooJob.getInstanceUrl())
+        && getJobName().equals(bambooJob.getJobName());
+  }
+
+  @Override
+  public int hashCode() {
+    int result = getInstanceUrl().hashCode();
+    result = 31 * result + getJobName().hashCode();
+    return result;
+  }
 }
