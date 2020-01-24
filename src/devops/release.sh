43b3e5bc12d84fce45f679aa8ea4cbe194e35f23
Courtney Peng <courtney.peng@gmail.com>
2020-01-24 16:28:02 -0500

    CICD setup on Github Actions

--- src/devops/release.sh
+++ src/devops/release.sh
@@ -2,7 +2,7 @@
 
 cp src/devops/.travis.settings.xml $HOME/.m2/settings.xml
 
-openssl aes-256-cbc -K $encrypted_2677e7731de9_key -iv $encrypted_2677e7731de9_iv -in src/devops/keys.gpg.enc -out keys.gpg -d
+openssl aes-256-cbc -K $encrypted_62fc69d7ec59_key -iv $encrypted_62fc69d7ec59_iv -in src/devops/keys.gpg.enc -out keys.gpg -d
 
 gpg --fast-import keys.gpg
 
